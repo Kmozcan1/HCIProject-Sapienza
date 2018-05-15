a5d528232a27b4f78bfea3ed8bb3899e603e6522
Kmozcan1 <kmozcan1@gmail.com>
2018-05-15 23:54:47 +0200

    AdminActivity, InsertProductActivity and layouts

--- app/src/main/java/com/example/onlinemarket/onlinemarket/AdminActivity.kt
+++ app/src/main/java/com/example/onlinemarket/onlinemarket/AdminActivity.kt
@@ -3,6 +3,7 @@ package com.example.onlinemarket.onlinemarket
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
 import kotlinx.android.synthetic.main.activity_admin.*
 
 class AdminActivity : AppCompatActivity() {
@@ -17,5 +18,11 @@ class AdminActivity : AppCompatActivity() {
             intent = Intent(this, ProductListActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_product, menu)
+        return true
     }
 }
--- app/src/main/java/com/example/onlinemarket/onlinemarket/ProductListActivity.kt
+++ app/src/main/java/com/example/onlinemarket/onlinemarket/ProductListActivity.kt
@@ -1,14 +1,19 @@
 package com.example.onlinemarket.onlinemarket
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.support.annotation.RequiresApi
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.View
 import android.view.WindowManager
-import com.example.onlinemarket.onlinemarket.R.id.product_listView
 import kotlinx.android.synthetic.main.activity_product_list.*
+import android.app.SearchManager
+import android.content.Context
+import android.view.Menu
+import android.widget.SearchView
+
+private var productListItemList = mutableListOf<ProductListItemData>()
 
 @RequiresApi(api = Build.VERSION_CODES.O)
 class ProductListActivity : AppCompatActivity() {
@@ -16,6 +21,8 @@ class ProductListActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_list)
+
+
 
         //Make the screen non-interactive & progressBar visible
         window.setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
@@ -37,18 +44,32 @@ class ProductListActivity : AppCompatActivity() {
         })
     }
 
+    override fun onNewIntent(intent: Intent) {
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent) {
+
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY)
+            applySearchFilter(productListItemList, query)
+        }
+    }
+
+
+
 
 
     //Fetches the products, pairs them with company images
     //and fills the listView
     private fun fillProductSearchList(companyMap: MutableMap<String, String>) {
-        val productListItemList = mutableListOf<ProductListItemData>()
         Utilities.getProducts(object: FireBaseListener {
             override fun onCallBack(value: Any) {
+                productListItemList.clear()
                 val products = value as ArrayList<Product?>
                 for (product in products) {
                     val productListItem = ProductListItemData(product!!.productName,
-                            product!!.productImage, companyMap[product!!.company])
+                            product!!.productImage, product.company, companyMap[product!!.company])
                     productListItemList.add(productListItem)
                 }
                 val productSearchListViewAdapter = ProductSearchListViewAdapter(baseContext,
@@ -62,6 +83,53 @@ class ProductListActivity : AppCompatActivity() {
         })
     }
 
-/*data class ProductListItemData(val productID: String, val ProductName: String,
-                               val productImage: String, var companyImage: String)*/
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_listproduct, menu)
+        val searchMenuItem = menu.findItem(R.id.search) // get my MenuItem with placeholder submenu
+        searchMenuItem.expandActionView() // Expand the search menu item in order to show by default the query
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(componentName))
+
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+            //TODO: Try to make this async
+            override fun onQueryTextChange(newText: String): Boolean {
+                if(newText == "") {
+                    val productSearchListViewAdapter = ProductSearchListViewAdapter(baseContext,
+                            R.layout.productsearch_list_item, productListItemList)
+                    product_listView.adapter = productSearchListViewAdapter
+                }
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+                //Task HERE
+                return false
+            }
+
+        })
+
+        return true
+    }
+
+    //Applies the filter and changes the adapter list
+    private fun applySearchFilter(list: MutableList<ProductListItemData>, query: String) {
+        if (query != "") {
+            val filteredList = mutableListOf<ProductListItemData>()
+            for (item in list) {
+                if (item.productName.toLowerCase().contains(query.toLowerCase()) || item.companyName.toLowerCase().contains(query.toLowerCase())) {
+                    filteredList.add(item)
+                }
+            }
+            val productSearchListViewAdapter = ProductSearchListViewAdapter(baseContext,
+                    R.layout.productsearch_list_item, filteredList)
+            product_listView.adapter = productSearchListViewAdapter
+        }
+    }
+
+
 }
